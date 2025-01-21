2a0455c99ec200b2c817a9536f61dbbe84ba3f3b
tanaprog <tananaeva.art@gmail.com>
2025-01-21 13:47:03 +0300

    rewrite file-main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,91 @@
 const mailPath = './mail.php'
 
-document.querySelectorAll('.uniForm').forEach((e) => {
+const middleForm = document.querySelector('.middle-form');
+const bottomForm = document.querySelector('.bottom-form');
+const inputRequire = document.querySelectorAll('.require');
+const inputRequireBottom = document.querySelectorAll('.require-bottom');
 
-	e.addEventListener('submit', function (e) {
+function sendMessage(e) {
+	e.preventDefault();
 
-		let th = this,
-			params = new FormData(this),
-			request = new XMLHttpRequest()
+	let error = formMiddleValidate(inputRequire);
 
-		request.open('POST', mailPath, true)
-		request.send(params)
+	let dataThis = this,
+		params = new FormData(this),
+		request = new XMLHttpRequest();
 
+	request.open('POST', mailPath, true);
+	request.send(params);
+
+	if (error === 0) {
 		request.onreadystatechange = function () {
 			if (this.readyState == 4 && this.status == 200) {
-				setTimeout(function () { th.reset() }, 1000)
-				alert('Thank you!')
+				setTimeout(function () { dataThis.reset() }, 1000)
+				alert('Спасибо, запрос отправлен!');
 			}
 		}
+	}
+}
 
-		e.preventDefault()
+function sendMessageFormBottom(e) {
+	e.preventDefault();
 
-	})
+	let error = formBottomValidate(inputRequire);
 
-})
+	let dataThis = this,
+		params = new FormData(this),
+		request = new XMLHttpRequest();
+
+	request.open('POST', mailPath, true);
+	request.send(params);
+
+	if (error === 0) {
+		request.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				setTimeout(function () { dataThis.reset() }, 1000)
+				alert('Спасибо, запрос отправлен!');
+			}
+		}
+	}
+}
+
+
+function formMiddleValidate(e) {
+	let error = 0;
+	for (let i = 0; i < inputRequire.length; i++) {
+		const elem = inputRequire[i];
+		formRemoveError(elem);
+		if (elem.value === '') {
+			formAddError(elem);
+			error++;
+		}
+	}
+	return error;
+}
+
+function formBottomValidate(e) {
+	let error = 0;
+	for (let i = 0; i < inputRequireBottom.length; i++) {
+		const elem = inputRequireBottom[i];
+		formRemoveError(elem);
+		if (elem.value === '') {
+			formAddError(elem);
+			error++;
+		}
+	}
+	return error;
+}
+
+function formAddError(input) {
+	input.classList.add('error');
+}
+function formRemoveError(input) {
+	input.classList.remove('error');
+}
+
+function init() {
+	middleForm.addEventListener('submit', sendMessage);
+	bottomForm.addEventListener('submit', sendMessageFormBottom);
+}
+
+init();
